c5b330f8c2f525dccc71617b8965c4fec6d3249d
SteveLim99 <stevelimzilian@gmail.com>
2020-10-08 01:35:35 +0800

    check permutation #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import com.company.arrays_and_strings.isUnique;
+import com.company.arrays_and_strings.checkPerm;
 
 public class Main {
 
@@ -9,8 +10,16 @@ public class Main {
         System.out.println("Optimized implementation: " + isUnique.checkUniqueOpti());
     }
 
+    public static void checkPermutation(String input, String compare){
+        checkPerm checkPerm = new checkPerm();
+        System.out.println(checkPerm.checkPermutation(input,compare));
+    }
+
     public static void main(String[] args) {
 	    // Arrays and Strings #1: isUnique
         // checkUnique("test")
+
+        // Arrays and Strings #2: Check Permutation
+        checkPermutation("","");
     }
 }
--- src/com/company/arrays_and_strings/checkPerm.java
+++ src/com/company/arrays_and_strings/checkPerm.java
@@ -1,4 +1,28 @@
 package com.company.arrays_and_strings;
 
 public class checkPerm {
+
+    public boolean checkPermutation(String input, String compare){
+        int inputLen = input.length();
+        int compareLen = compare.length();
+
+        if(inputLen != compareLen){
+            return false;
+        }
+
+        int inputCount = 0;
+        int compareCount = 0;
+
+        for(int i = 0; i < inputLen; i++){
+            int inputCurr = input.charAt(i);
+            int compareCurr = compare.charAt(i);
+            inputCount += inputCurr;
+            compareCount += compareCurr;
+        }
+
+        if (inputCount != compareCount){
+            return false;
+        }
+        return  true;
+    }
 }
