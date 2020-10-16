298245b5a83981b82504fd4a5c147a17f201a8a5
SteveLim99 <stevelimzilian@gmail.com>
2020-10-17 00:44:02 +0800

    delete middle node #2.3

--- src/com/company/linked_list/deleteMiddleNode.java
+++ src/com/company/linked_list/deleteMiddleNode.java
@@ -1,31 +1,32 @@
 package com.company.linked_list;
 
-public class removeDups {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode tmp = head;
+public class deleteMiddleNode {
+    public ListNode middleNode(ListNode head) {
         int counter = 0;
+        ListNode tmp = head;
         while(tmp != null){
             counter += 1;
             tmp = tmp.next;
         }
 
-        int index = counter - n;
-        if(index == 0) return head.next;
+        if(counter <= 2) return head;
+        int mid = 0;
+        if (counter % 2 == 0) mid = (counter/2)+1;
+        else mid = counter/2 ;
 
-        counter = 1;
         ListNode slow = head;
         ListNode fast = head.next;
+        int count = 0;
+
         while(fast != null){
-            if(counter == index){
+            if(count+1 == mid){
                 slow.next = fast.next;
                 break;
             }
             slow = slow.next;
             fast = fast.next;
-            counter++;
+            count++;
         }
-
         return head;
-
     }
 }
