30b75064e26211f9b5b200c7321babf01753111c
SteveLim99 <stevelimzilian@gmail.com>
2020-10-17 15:58:07 +0800

    intersection #2.7 -.7 - first

--- src/com/company/linked_list/intersection.java
+++ src/com/company/linked_list/intersection.java
@@ -1,5 +1,7 @@
 package com.company.linked_list;
 
+import java.util.HashMap;
+
 public class intersection {
     public ListNode cutNode(ListNode head, int toCut){
         for(int i = 0; i < toCut; i++){
@@ -9,6 +11,19 @@ public class intersection {
     }
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashMap<ListNode, Integer> dict = new HashMap<ListNode, Integer>();
+        while(headA != null){
+            dict.put(headA, 1);
+            headA = headA.next;
+        }
+        while(headB != null){
+            if(dict.get(headB) != null) return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    public ListNode getIntersectionNode_Opti(ListNode headA, ListNode headB) {
         ListNode endA = null;
         ListNode endB = null;
         ListNode tmpA = headA;
