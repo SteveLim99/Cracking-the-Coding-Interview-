982e1c4b70a49dc19f9862d82628dca3ff186cd4
SteveLim99 <stevelimzilian@gmail.com>
2020-10-22 01:36:00 +0800

    is subtrpath sum  -o

---
+++ src/com/company/treesAndGraphs/pathSum.java
@@ -0,0 +1,9 @@
+package com.company.treesAndGraphs;
+
+public class pathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        sum -= root.val;
+        return (root.left == null && root.right == null && sum == 0) || hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
