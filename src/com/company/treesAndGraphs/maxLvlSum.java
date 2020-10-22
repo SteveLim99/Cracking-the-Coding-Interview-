a6d6276d21dab6a934f7247f15167e32f78413f3
SteveLim99 <stevelimzilian@gmail.com>
2020-10-22 23:34:03 +0800

    max level sum of btree -bfs

--- src/com/company/treesAndGraphs/maxLvlSum.java
+++ src/com/company/treesAndGraphs/maxLvlSum.java
@@ -1,7 +1,9 @@
 package com.company.treesAndGraphs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class maxLvlSum {
     public int maxLevelSum_1(TreeNode root) {
@@ -24,6 +26,39 @@ public class maxLvlSum {
             }
             level++;
             currLevel = newLevel;
+        }
+        return maxLvl;
+    }
+
+    public int maxLevelSum(TreeNode root) {
+        Queue queue = new LinkedList();
+        queue.add(root);
+        int count = 1;
+        int level = 1;
+        int max = root.val;
+        int maxLvl = 1;
+        while(!queue.isEmpty()){
+            int sum = 0;
+            int nodes = 0;
+            while(count != 0){
+                TreeNode curr = (TreeNode) queue.remove();
+                sum += curr.val;
+                if(curr.left != null){
+                    queue.add(curr.left);
+                    nodes++;
+                }
+                if(curr.right != null) {
+                    queue.add(curr.right);
+                    nodes++;
+                }
+                count--;
+            }
+            if(sum > max){
+                max = sum;
+                maxLvl = level;
+            }
+            level++;
+            count = nodes;
         }
         return maxLvl;
     }
