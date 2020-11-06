a687a72af70049663860b028aa7215e39c2dd29b
SteveLim99 <stevelimzilian@gmail.com>
2020-11-06 23:37:35 +0800

    paren opti

--- src/com/company/recursionAndDp/parens.java
+++ src/com/company/recursionAndDp/parens.java
@@ -24,4 +24,28 @@ public class parens {
             backtracing_unoptimized(output, tmp+")",left,right+1,max);
         }
     }
+
+    public List<String> generateParenthesis_opti(int n) {
+        List<String> output = new ArrayList();
+        backtracing_opti(output, new StringBuilder(),0,0,n);
+        return output;
+    }
+
+    private void backtracing_opti(List<String> output, StringBuilder sb, int left, int right, int max){
+        if(sb.length() == max*2){
+            output.add(sb.toString());
+            return;
+        }
+
+        if(left < max){
+            backtracing_opti(output, sb.append("("),left+1,right,max);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        if (right < left){
+            backtracing_opti(output, sb.append(")"),left,right+1,max);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+
 }
