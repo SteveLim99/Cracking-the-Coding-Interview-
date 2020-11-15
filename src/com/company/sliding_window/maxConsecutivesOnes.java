79aa5ef86b4e10bb4544323c3d70690b0bf68350
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2020-11-16 00:24:58 +0800

    max consecv ones sliding windown opti

---
+++ src/com/company/sliding_window/maxConsecutivesOnes.java
@@ -0,0 +1,12 @@
+package com.company.sliding_window;
+
+public class maxConsecutivesOnes {
+    public int longestOnes(int[] A, int K) {
+        int i = 0, j;
+        for (j = 0; j < A.length; ++j) {
+            if (A[j] == 0) K--;
+            if (K < 0 && A[i++] == 0) K++;
+        }
+        return j - i;
+    }
+}
