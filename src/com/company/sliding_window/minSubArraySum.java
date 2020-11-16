55a4b846cec2f17e3da90aa9e7c66fb1217de13a
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2020-11-16 18:34:17 +0800

    min subarray sum cleaned

--- src/com/company/sliding_window/minSubArraySum.java
+++ src/com/company/sliding_window/minSubArraySum.java
@@ -4,15 +4,13 @@ public class minSubArraySum {
     public int minSubArrayLen(int s, int[] nums) {
         if(nums.length == 0) return 0;
         int i = 0, j, count = 0, max = 2147483647;
-        boolean foundSum = false;
         for(j = 0; j < nums.length; j++){
             count += nums[j];
-            if(count >= s) foundSum = true;
             while(count >= s){
-                if(count >= s) max = Math.min(max,j-i+1);
+                max = Math.min(max,j-i+1);
                 count -= nums[i++];
             }
         }
-        return foundSum ? max : 0;
+        return max!=2147483647 ? max : 0;
     }
 }
