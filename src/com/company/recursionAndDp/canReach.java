b1b854749475de3e4dd9b0f4aaa92e1614fbfc19
SteveLim99 <stevelimzilian@gmail.com>
2020-11-21 03:32:08 +0800

    can jump shit

--- src/com/company/recursionAndDp/canReach.java
+++ src/com/company/recursionAndDp/canReach.java
@@ -11,4 +11,22 @@ public class canReach {
         arr[start] = -arr[start];
         return dfs(arr,start+arr[start]) || dfs(arr,start-arr[start]);
     }
+
+    public boolean canJump(int[] nums) {
+        return dfs_canJump(nums,0);
+    }
+
+    private boolean dfs_canJump(int[] nums, int curr){
+        if(curr >= nums.length || nums[curr] == -1) return false;
+        if(curr == nums.length-1) return true;
+        if(nums[curr] == 0) return false;
+
+        boolean tmp = false;
+        for(int i = 1; i <= nums[curr]; i++){
+            tmp = tmp || dfs_canJump(nums,curr+i);
+        }
+        nums[curr] = -1;
+
+        return tmp;
+    }
 }
