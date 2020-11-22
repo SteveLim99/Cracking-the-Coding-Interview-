b72adc93d389237d0678c5e025f4b66db57560d4
SteveLim99 <stevelimzilian@gmail.com>
2020-11-22 14:29:17 +0800

    increasing subsequence -opti

--- src/com/company/recursionAndDp/increasingSubsequence.java
+++ src/com/company/recursionAndDp/increasingSubsequence.java
@@ -1,7 +1,9 @@
 package com.company.recursionAndDp;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class increasingSubsequence {
     public List<List<Integer>> findSubsequences(int[] nums) {
@@ -22,6 +24,26 @@ public class increasingSubsequence {
             }
             backtrack(nums,total,tmp,i+1);
             if(added) tmp.remove(tmp.size()-1);
+        }
+    }
+
+    public List<List<Integer>> findSubsequences_opt(int[] nums) {
+        List<List<Integer>> total = new ArrayList();
+        dfs(nums,total,new ArrayList(),0);
+        return total;
+    }
+
+    private void dfs(int[] nums, List<List<Integer>> total, List<Integer> tmp, int head){
+        if(tmp.size() >= 2) total.add(new ArrayList(tmp));
+        Set<Integer> newNums = new HashSet();
+        for(int i = head; i < nums.length; i++){
+            if(newNums.contains(nums[i])) continue;
+            if(tmp.size() == 0 || nums[i] >= tmp.get(tmp.size()-1)){
+                tmp.add(nums[i]);
+                newNums.add(nums[i]);
+                dfs(nums,total,tmp,i+1);
+                tmp.remove(tmp.size()-1);
+            }
         }
     }
 }
