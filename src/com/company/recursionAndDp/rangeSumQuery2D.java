fdd81178979c7b6fb6d80a35268d1942710d9afb
SteveLim99 <stevelimzilian@gmail.com>
2020-11-29 23:08:36 +0800

    range sum query 2d -opti

--- src/com/company/recursionAndDp/rangeSumQuery2D.java
+++ src/com/company/recursionAndDp/rangeSumQuery2D.java
@@ -2,16 +2,32 @@ package com.company.recursionAndDp;
 
 public class rangeSumQuery2D {
     int[][] matrix;
-    int[][]df;
+    int[][] dp;
     int rows;
     int cols;
 
-    public rangeSumQuery2D(int[][] matrix) {
+    public rangeSumQuery2D(int[][] matrix, boolean placeHolder) {
         if(matrix.length > 0 && matrix[0].length > 0) {
             this.matrix = matrix;
             this.rows = matrix.length;
             this.cols = matrix[0].length;
         }
+    }
+
+    public rangeSumQuery2D(int[][] matrix) {
+        if(matrix.length > 0 && matrix[0].length > 0) {
+            dp = new int[matrix.length+1][matrix[0].length+1];
+            for(int i = 1; i <= matrix.length; i++ ){
+                for(int j = 1; j <= matrix[0].length; j++){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1];
+
+                }
+            }
+        }
+    }
+
+    public int sumRegion_opti(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
     }
 
     public int sumRegion_1(int row1, int col1, int row2, int col2) {
