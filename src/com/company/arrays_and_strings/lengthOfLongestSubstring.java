b1e1d8f72f6413af2496c087c60193577750a0c5
SteveLim99 <stevelimzilian@gmail.com>
2020-12-20 02:54:47 +0800

    longest substring -2

---
+++ src/com/company/arrays_and_strings/lengthOfLongestSubstring.java
@@ -0,0 +1,35 @@
+package com.company.arrays_and_strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class lengthOfLongestSubstring {
+    public int lengthOfLongestSubstring_2(String s) {
+        Set<Character> unique = new HashSet();
+        int i, j = 0, tmp = 0, max = 0;
+
+        for(i = 0; i < s.length(); i++){
+            char curr = s.charAt(i);
+
+            if(unique.contains(curr)){
+                boolean found = false;
+                while(!found){
+                    char tail = s.charAt(j);
+                    unique.remove(tail);
+                    if(tail == curr) found = true;
+                    j++;
+                }
+                if(tmp > max) max = tmp;
+                tmp = 1 + i - j;
+                unique.add(curr);
+            }
+
+            else {
+                unique.add(curr);
+                max = Math.max(++tmp,max);
+            }
+        }
+
+        return max;
+    }
+}
