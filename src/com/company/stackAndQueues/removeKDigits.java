ef0ea15297091cb6db2a4bd300b9ce0a331adedb
SteveLim99 <stevelimzilian@gmail.com>
2020-12-28 16:23:24 +0800

    remove k digits -2 dequeue

--- src/com/company/stackAndQueues/removeKDigits.java
+++ src/com/company/stackAndQueues/removeKDigits.java
@@ -1,5 +1,7 @@
 package com.company.stackAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class removeKDigits {
@@ -33,5 +35,34 @@ public class removeKDigits {
         }
 
         return sb.reverse().toString();
+    }
+
+    public String removeKdigits_2(String num, int k) {
+        if(k >= num.length()) return "0";
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : num.toCharArray()) {
+            while(k > 0 && !stack.isEmpty() && stack.peekLast() > c) {
+                stack.removeLast();
+                k--;
+            }
+            stack.addLast(c);
+        }
+
+        while(k>0) {
+            stack.removeLast();
+            k--;
+        }
+
+        // Remove all zeros from the front of the stack and then if stack is empty, return "0"
+        while(!stack.isEmpty() && stack.peekFirst()== '0') stack.removeFirst();
+        if(stack.isEmpty()) return "0";
+
+        // build the number from the stack
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.removeFirst());
+        }
+        return sb.toString();
     }
 }
