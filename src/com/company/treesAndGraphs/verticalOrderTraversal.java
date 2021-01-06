210eeee91c9d570290cc1e0b24d3617bf9dff705
SteveLim99 <stevelimzilian@gmail.com>
2021-01-06 16:30:01 +0800

    vertical order traversal -o

--- src/com/company/treesAndGraphs/verticalOrderTraversal.java
+++ src/com/company/treesAndGraphs/verticalOrderTraversal.java
@@ -84,4 +84,49 @@ public class verticalOrderTraversal {
 
         return res;
     }
+
+    public List<List<Integer>> verticalOrder_3(TreeNode root) {
+        HashMap<Integer, List<Integer>> map = new HashMap();
+        List<List<Integer>> res = new ArrayList();
+        Queue<TreeNode> bfs = new LinkedList();
+        Queue<Integer> xMap = new LinkedList();
+        int xMin = 0;
+        int xMax = 0;
+        if(root != null) {
+            bfs.offer(root);
+            xMap.offer(0);
+        }
+
+        while(!bfs.isEmpty()){
+            int count = bfs.size();
+
+            for(int i = 0; i < count; i++){
+                TreeNode curr = bfs.poll();
+                int x = xMap.poll();
+                List<Integer> tmp = map.getOrDefault(x, new ArrayList());
+                tmp.add(curr.val);
+                map.put(x,tmp);
+
+                if(curr.left != null){
+                    bfs.offer(curr.left);
+                    xMap.offer(x-1);
+                    if(x-1 < xMin) xMin = x-1;
+                }
+
+                if(curr.right != null){
+                    bfs.offer(curr.right);
+                    xMap.offer(x+1);
+                    if(x+1 > xMax) xMax = x+1;
+                }
+            }
+        }
+
+        for(int i = xMin; i <= xMax; i++){
+            if(map.containsKey(i)){
+                res.add(map.get(i));
+            }
+        }
+
+        return res;
+    }
 }
